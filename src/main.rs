8ab841e0203c896af37e8f29e9d08d8df4039a2f
lucarge <luca.argenziano@icloud.com>
2018-11-23 09:05:47 +0100

    Bootstrap API and database

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,39 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
 
-#[get("/<name>/<age>")]
-fn hello(name: String, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
+use rocket_contrib::{Json, Value};
+
+mod hero;
+use hero::{Hero};
+
+#[delete("/<id>")]
+fn delete(id: i32) -> Json<Value> {
+  Json(json!({
+    "status": "ok",
+  }))
+}
+
+#[get("/")]
+fn read() -> Json<Value> {
+  Json(json!(["hero 1", "hero 2"]))
+}
+
+#[post("/", data="<hero>")]
+fn create(hero: Json<Hero>) -> Json<Hero> {
+  hero
+}
+
+#[put("/<id>", data="<hero>")]
+fn update(id: i32, hero: Json<Hero>) -> Json<Hero> {
+  hero
 }
 
 fn main() {
     rocket::ignite()
-        .mount("/hello", routes![hello])
+        .mount("/hero", routes![create, update, delete])
+        .mount("/heroes", routes![read])
         .launch();
 }
